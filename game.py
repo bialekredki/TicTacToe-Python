bcaa759fae97533fbca7381766bf1efb8505327c
Oskar <oskarkorgul@gmail.com>
2020-12-27 20:31:35 +0100

    Separetd GUI and game's logic. Added local for easier localisation. Removed constants from game.py to consts.py.

--- game.py
+++ game.py
@@ -1,165 +1,84 @@
 # ----------------------IMPORTS---------------------------------------
 import math
 import time
-
+import copy
 import pygame as pyg
-
-
-import centeredText as ctxt
+import ttt_gfx
 import ttt_ai
 import ttt_board
 import ttt_console
 
-
-# ----------------------------END OF IMPORTS--------------------------
-
-def wait():
-    close = False
-    while not close:
-        for event in pyg.event.get():
-            if event.type == pyg.MOUSEBUTTONDOWN:
-                close = True
-            elif event.type == pyg.QUIT:
-                closed = True
-                pyg.display.quit()
-                pyg.quit()
-
-
-def setResolution(str_value:str,value:int):
-    global width, height
-    if value == 0:
-        width = 800
-        height = 600
-    elif value == 1:
-        width = 1280
-        height = 720
-    elif value == 2:
-        width = 1280
-        height = 1024
-
-def drawSideBar(main_font:pyg.font,turn:int):
-    display.blit(main_font.render("MENU", True, WHITE), (0,0))
-    menu_button_size = main_font.size("MENU")
-    if turn == 1:   xo = 'X'
-    else : xo = 'O'
-    display.blit(main_font.render("Player {}'s turn({})".format(turn,xo), True, WHITE), (0,menu_button_size[1]))
-    display.blit(main_font.render("Player 1 : {}".format(score[1]), True, WHITE), (width//2, 0))
-    display.blit(main_font.render("Player 2 : {}".format(score[2]), True, WHITE), (width // 2, menu_button_size[1]))
-    display.blit(main_font.render("Draw : {}".format(score[0]), True, WHITE), (width // 2, menu_button_size[1]*2))
-
-    return menu_button_size
-
-
-def mainMenu(main_font:pyg.font, menu_button_size):
-    close = False
-    option = 0
-    local_resolution = (width, height)
-    local_resolution_option = resolution
-    local_size = start_size
-    local_mode = game_mode
-    local_console = console
-
-    while not close:
-        for event in pyg.event.get():
-            if event.type == pyg.MOUSEBUTTONDOWN:
-                close = True
-            if event.type == pyg.KEYDOWN:
-                if event.key == pyg.K_RIGHT:
-                    if option+1 > len(menu_button_size):
-                        option = 0
-                    else:
-                        option += 1
-                if event.key == pyg.K_ESCAPE:
-                    close = True
-        display.fill(BLACK)
-        # -----------------------------------------------UNCHANGAEBLE PART---------------------------------------------
-        display.blit(main_font.render("Use left and right arrow keys to navigate between options", True, WHITE), (0,0))
-        display.blit(main_font.render("Use up and down arrow keys to change settings", True, WHITE), (0, menu_button_size[1]))
-        display.blit(main_font.render("Press mouse button or ESC to exit menu", True, WHITE), (0,menu_button_size[1]*2))
-        display.blit(main_font.render("Current option: {}".format(main_menu_options[option]),True,WHITE), (0,menu_button_size[1]*3))
-
-        # ---------------------------------------------OPTIONS-----------------------------------------------------------
-        if option == 0:
-            display.blit(main_font.render(resolution_options[local_resolution_option], True, WHITE), (0,menu_button_size[1]*4))
-        elif option == 1:
-            display.blit(main_font.render(game_mode_options[local_mode], True, WHITE),
-                         (0, menu_button_size[1] * 4))
-        elif option == 2:
-            display.blit(main_font.render("{}".format(local_size), True, WHITE), (0,menu_button_size[1]*4))
-        pyg.display.update()
-
-    # apply changes
-
-def endMenu(result:int=0):
-    pass
-
-def drawGrid(grid_params, board, xo_font):
-    state = board.getState()
-    for x in range(board.size):
-        for y in range(board.size):
-            rect = pyg.Rect(grid_params["block size"] * x + grid_params["start x"],
-                            grid_params["block size"] * y + grid_params["start y"],
-                            grid_params["block size"], grid_params["block size"])
-            pyg.draw.rect(display, WHITE, rect, 1)
-            if state[x + y * board.size] == 1:
-                ctxt.selfcenteredText(xo_font, "X", rect.topleft, grid_params["block size"]).draw(display, WHITE)
-            elif state[x + y * board.size] == 2:
-                ctxt.selfcenteredText(xo_font, "O", rect.topleft, grid_params["block size"]).draw(display, WHITE)
-            else:
-                continue
-
-
-def mousePositionToGridTile(grid_params:dict, mouse_pos:list, size:int):
-    for x in range(size):
-        for y in range(size):
-            if (grid_params["block size"] * x + grid_params["start x"] < mouse_pos[0] < grid_params["block size"] * x +
-                    grid_params["start x"] + grid_params[
-                        "block size"] and
-                    grid_params["block size"] * y + grid_params["start y"] < mouse_pos[1] < grid_params[
-                        "block size"] * y + grid_params["start y"] + grid_params["block size"]):
-                return x + y * size
-
-
-# --------------MAIN FUNCTION FOR GRAPHICAL TIC TAC TOE---------------
-def run():
-    # ---------------------initialization-----------------------------
-    global game_number
-    pyg.display.set_caption("TicTacToe")
-    closed = False
-    clock = pyg.time.Clock()
-    b = ttt_board.Board(size=start_size)
-    turn = 1
-    xo_font = pyg.font.SysFont("Comic Sans", 270 - 70 * (b.size - 3))
-    grid_params = {
-        "width": 3 * width // 4,
-        "height": 3 * height // 4
-    }
-    grid_params["start x"] = (width - grid_params["width"]) // 2
-    grid_params["start y"] = (height - grid_params["height"]) // 2
-    grid_params["block size"] = grid_params["height"]//b.size
-    print(grid_params["width"])
-    print(grid_params["height"])
-    print(grid_params["start x"])
-    print(grid_params["start y"])
-    print(grid_params["block size"])
-    # ---------------------end of initialization----------------------
-    if game_number % 2 == 0:
-        turn = 1
+def process_menu_event(event:pyg.event,current_option:int,option_set:tuple):
+    last_options = [3,6,2,1,1]
+    if event.type == pyg.KEYDOWN:
+        if event.key == pyg.K_UP:
+            if current_option-1 >= 0:
+                return current_option - 1
+            else: return len(last_options) - 1
+        elif event.key == pyg.K_DOWN:
+            if current_option+1 <= len(last_options)-1:
+                return current_option + 1
+            else: return 0
+        elif event.key == pyg.K_LEFT:
+            if option_set[current_option] - 1 >= 0:
+                option_set[current_option] = option_set[current_option] - 1
+            else: option_set[current_option] = last_options[current_option]
+            return current_option
+        elif event.key == pyg.K_RIGHT:
+            if option_set[current_option] + 1 <= last_options[current_option]:
+                option_set[current_option] = option_set[current_option] + 1
+            else: option_set[current_option] = 0
+            return current_option
+        elif event.key == pyg.K_ESCAPE:
+            return -1
+        elif event.key == pyg.K_RETURN:
+            return None
     else:
-        turn = 2
+        return current_option
 
+
+def read_local():
+    local_file = open("local",'r')
+    local = dict()
+    lines = local_file.readlines()
+    local_file.close()
+    for line in lines:
+        split = line.split(';')
+        for index, s in enumerate(split):
+            if s.endswith('\n'):
+                split[index] = s.replace('\n','')
+        if not split[0].startswith('opt'):
+            local[split[0]] = split[1]
+        else:
+            opt_list = list()
+            for index,element in enumerate(split):
+                if index != 0:
+                    opt_list.append(element)
+            local[split[0]] = opt_list
+    return local
+
+def run():
+    closed = False
+    local = read_local()
+    b = ttt_board.Board(size=start_size)
+    window = ttt_gfx.GameWindow(resolutions[0], local, b.size)
+    turn = 1
+    menu = False
+    game_number = 0
+    option = 0
+    option_set = [0,0,0,0,0]
+    previous_options_set = copy.deepcopy(option_set)
     ai1 = ttt_ai.AI(1)
     ai2 = ttt_ai.AI(2)
-    space = False
     # -------------------------main loop------------------------------
     while not closed:
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
+                window.close()
                 closed = True
-                pyg.display.quit()
-                pyg.quit()
             elif event.type == pyg.MOUSEBUTTONDOWN and turn == 2:
-                pos = mousePositionToGridTile(grid_params, pyg.mouse.get_pos(), b.size)
+                pos = ttt_gfx.mousePositionToGridTile(window.calculate_grid_parameters(b.size), pyg.mouse.get_pos(), b.size)
+                menu_button_size = window.get_menu_button_size()
                 if pos is not None:
                     b.update(turn, pos)
                     if turn == 1:
@@ -167,25 +86,33 @@ def run():
                     else:
                         turn = 1
                 elif pyg.mouse.get_pos()[0] < menu_button_size[0] and pyg.mouse.get_pos()[1] < menu_button_size[1]:
-                    print("XD")
-                    mainMenu(main_font,menu_button_size)
+                    menu = True
+                elif pyg.mouse.get_pos()[0] < menu_button_size[0] and pyg.mouse.get_pos()[1] < menu_button_size[1]*3:
+                    window.close()
+            elif menu:
+                option = process_menu_event(event, option,option_set)
+                if option is None:
+                    menu = False
+                    option = 0
+                    if option_set[0] != previous_options_set[0] or option_set[4] != previous_options_set[4]:
+                        window.resize(resolutions[option_set[0]],bool(option_set[4]))
+                    if option_set[1] != previous_options_set[1]:
+                        pass
+                    if option_set[2] != previous_options_set[2]:
+                        pass
+                    if option_set[3] != previous_options_set[3]:
+                        pass
+                elif option == -1:
+                    option_set = copy.deepcopy(previous_options_set)
 
-        display.fill(BLACK)
-        drawGrid(grid_params, b, xo_font)
-        menu_button_size = drawSideBar(main_font,turn)
-        pyg.display.update()
-
-        clock.tick(60)
-
+        if not menu:
+            window.render(board=b,game_params=(turn,score))
+        else:
+            window.render(menu_params=(option,option_set[0],option_set[1],option_set[2],option_set[3],option_set[4]))
         current_result = b.checkForEndgame()
         if current_result != -1:
             score[current_result] += 1
             game_number = game_number + 1
-            if current_result > 0:
-                b.display()
-            clock.tick(60)
-            pyg.display.update()
-            # wait()
             return 0
 
         if turn == 1:
@@ -204,37 +131,17 @@ def run():
 
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
-img_x = 0
-img_o = 0
-width = 800
-height = 600
 score = [0,0,0]
-fullscreen = False
-game_mode = 0
-console = False
-game_number = 0
-start_size = 0
-resolution = 0
-
-main_menu_options = ("Resolution", "Game mode", "Board's size", "Enable console")
-resolution_options = ("800x600", "1280x720", "1280x1024")
-resolutions = ((800,600),(1270,720),(1280,1024))
-game_mode_options = ("Player x AI", "Player x Player", "AI x AI")
+fullscreen = bool()
+game_mode = int()
+console = bool()
+game_number = int()
+start_size = int()
+resolution = int()
+resolutions = ((800,600),(1270,720),(1280,1024),(0,0))
 # -------------------------END OF GLOBAL VARIABLES--------------------
 
 config = open("config", 'r')
 for x in config:    exec(x)
 config.close()
-width = resolutions[resolution][0]
-height = resolutions[resolution][1]
-while True:
-    if pyg.display is None: break
-    if not console:
-        pyg.init()
-        display = pyg.display.set_mode((width, height))
-        pyg.font.init()
-        main_font = pyg.font.SysFont("Comic Sans", 30)
-        run()
-    else:
-        if ttt_console.run() == 0: break
-exit()
+run()
---
+++ ttt_gfx.py
@@ -0,0 +1,149 @@
+"""
+    TTT_GFX_PY_
+    TicTacToe-Graphics module
+    Responsible for rendering and handling game's graphics.
+    AUTHOR OSKAR KORGUL
+"""
+
+import pygame as pyg
+import consts
+import ttt_board
+import centeredText as centxt
+
+
+def determine_colour(option:int, value:int):
+    if option == value:
+        return consts.CRIMSON
+    else: return consts.WHITE
+
+
+class GameWindow():
+    def __init__(self,resolution:tuple,local:dict,board_size:int,frame_rate:int=60):
+        self.local = local
+        self.display = pyg.display.set_mode(resolution)
+        pyg.display.set_caption(local["window"])
+        pyg.font.init()
+        self.main_font = pyg.font.SysFont("Comic Sans", 30)
+        self.xo_font = pyg.font.SysFont("Comic Sans", 270 - 70 * (board_size - 3))
+        self.clock = pyg.time.Clock()
+        self.frame_rate = frame_rate
+
+    def calculate_grid_parameters(self,board_size:int,resolution:tuple=None):
+        if resolution is None:
+            resolution = self.display.get_size()
+        grid_params = {
+            "width": 3 * resolution[0] // 4,
+            "height": 3 * resolution[1] // 4
+        }
+        grid_params["start x"] = (resolution[0] - grid_params["width"]) // 2
+        grid_params["start y"] = (resolution[1] - grid_params["height"]) // 2
+        grid_params["block size"] = grid_params["height"] // board_size
+        return grid_params
+
+    def draw_grid(self,grid_params, board):
+        state = board.getState()
+        for x in range(board.size):
+            for y in range(board.size):
+                rect = pyg.Rect(grid_params["block size"] * x + grid_params["start x"],
+                                grid_params["block size"] * y + grid_params["start y"],
+                                grid_params["block size"], grid_params["block size"])
+                pyg.draw.rect(self.display, consts.WHITE, rect, 1)
+                if state[x + y * board.size] == 1:
+                    centxt.selfcenteredText(self.xo_font, "X", rect.topleft, grid_params["block size"]).draw(self.display, consts.WHITE)
+                elif state[x + y * board.size] == 2:
+                    centxt.selfcenteredText(self.xo_font, "O", rect.topleft, grid_params["block size"]).draw(self.display, consts.WHITE)
+                else:
+                    continue
+
+    def draw_side_bar(self, turn: int, score:tuple):
+        self.display.blit(self.main_font.render(self.local["menu"], True, consts.WHITE), (0, 0))
+        menu_button_size = self.main_font.size("MENU")
+        if turn == 1:
+            xo = 'X'
+        else:
+            xo = 'O'
+        self.display.blit(self.main_font.render(self.local["players_turn"].format(turn, xo), True, consts.WHITE), (0, menu_button_size[1]))
+        self.display.blit(self.main_font.render(self.local["player1_score"].format(score[1]), True, consts.WHITE), (self.display.get_width() // 2, 0))
+        self.display.blit(self.main_font.render(self.local["player2_score"].format(score[2]), True, consts.WHITE), (self.display.get_width() // 2, menu_button_size[1]))
+        self.display.blit(self.main_font.render(self.local["draw_score"].format(score[0]), True, consts.WHITE), (self.display.get_width() // 2, menu_button_size[1] * 2))
+        self.display.blit(self.main_font.render(self.local["exit"], True, consts.CRIMSON), (0,menu_button_size[1]*2))
+
+
+    def get_menu_button_size(self):
+        return self.main_font.size("MENU")
+
+    def resize(self,resolution:tuple,fullscreen:bool=False):
+        if fullscreen:
+            self.display = pyg.display.set_mode(resolution,flags=pyg.FULLSCREEN)
+        else:
+            self.display = pyg.display.set_mode(resolution)
+
+    def render(self,board:ttt_board.Board=None,game_params:tuple=None,menu_params:tuple=None):
+        self.display.fill(consts.BLACK)
+        if board is not None and game_params is not None:
+            self.draw_grid(self.calculate_grid_parameters(board.size),board)
+            self.draw_side_bar(game_params[0],game_params[1])
+        elif menu_params is not None:
+            self.draw_main_menu(menu_params)
+        pyg.display.update()
+        self.clock.tick(60)
+
+    def draw_main_menu(self,menu_params):
+        # menu_params :
+        # 0 -> current selection
+        # 1 -> resolution option current
+        # 2 -> board size current
+        # 3 -> game mode current
+        # 4 -> console current
+        menu_button_size = self.get_menu_button_size()
+        self.display.blit(self.main_font.render(self.local["menu_instruction_1"], True, consts.WHITE),(0, 0))
+        self.display.blit(self.main_font.render(self.local["menu_instruction_2"], True, consts.WHITE),(0, menu_button_size[1]))
+        self.display.blit(self.main_font.render(self.local["menu_instruction_3"], True, consts.WHITE),(0, menu_button_size[1] * 2))
+        self.display.blit(
+            self.main_font.render("{} : {}".format(self.local["menu_resolution"],self.local["opt_resolution"][menu_params[1]]),
+            True, determine_colour(menu_params[0],0)), (0,menu_button_size[1]*3))
+        self.display.blit(
+            self.main_font.render("{} : {}".format(self.local["menu_board_size"], self.local["opt_board_size"][menu_params[2]]),
+            True, determine_colour(menu_params[0], 1)),(0,menu_button_size[1]*4))
+        self.display.blit(
+            self.main_font.render("{} : {}".format(self.local["menu_game_mode"], self.local["opt_game_mode"][menu_params[3]]),
+            True, determine_colour(menu_params[0], 2)),(0,menu_button_size[1]*5))
+        self.display.blit(
+            self.main_font.render("{} : {}".format(self.local["menu_console"], self.local["opt_console"][menu_params[4]]),
+            True, determine_colour(menu_params[0], 3)),(0,menu_button_size[1]*6))
+        self.display.blit(
+            self.main_font.render(
+                "{} : {}".format(self.local["menu_fullscreen"], self.local["opt_fullscreen"][menu_params[5]]),
+                True, determine_colour(menu_params[0], 4)), (0, menu_button_size[1] * 7))
+
+    def close(self,to_console:bool=False):
+        pyg.display.quit()
+        pyg.quit()
+        if not to_console:
+            exit()
+
+
+
+def wait():
+    close = False
+    while not close:
+        for event in pyg.event.get():
+            if event.type == pyg.MOUSEBUTTONDOWN:
+                close = True
+            elif event.type == pyg.QUIT:
+                close = True
+                pyg.display.quit()
+                pyg.quit()
+
+
+
+
+def mousePositionToGridTile(grid_params:dict, mouse_pos:tuple, size:int):
+    for x in range(size):
+        for y in range(size):
+            if (grid_params["block size"] * x + grid_params["start x"] < mouse_pos[0] < grid_params["block size"] * x +
+                    grid_params["start x"] + grid_params[
+                        "block size"] and
+                    grid_params["block size"] * y + grid_params["start y"] < mouse_pos[1] < grid_params[
+                        "block size"] * y + grid_params["start y"] + grid_params["block size"]):
+                return x + y * size
